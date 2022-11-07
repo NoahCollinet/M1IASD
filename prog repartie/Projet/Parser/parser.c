89d6e7d037ae70f48c3beae39f0b6599b1b3307d
Bettendroffer Dylan <dylan.bettendroffer@etu.umontpellier.fr>
2022-11-07 14:37:04 +0100

    Correction dans l'indentation

--- prog repartie/Projet/Parser/parser.c
+++ prog repartie/Projet/Parser/parser.c
@@ -8,11 +8,9 @@ void parse(FILE *file, int *vertices, int **edges) {
   char c;
 
   while ((c = fgetc(file)) != EOF) {
-    printf("%c", c);
     if (c == 'c') {
       while (c != '\n') {
         c = fgetc(file);
-        printf("%c", c);
       }
     }
 
@@ -20,7 +18,6 @@ void parse(FILE *file, int *vertices, int **edges) {
 
       while (!IS_NUM(c)) {
         c = fgetc(file);
-        printf("%c here", c);
       }
 
       char num[20];
@@ -29,79 +26,73 @@ void parse(FILE *file, int *vertices, int **edges) {
       while (IS_NUM(c)) {
         num[i] = c;
         c = fgetc(file);
-        printf("%c", c);
         ++i;
       }
 
       num[i] = '\0';
 
+      printf("%s\n", num);
       edges = malloc(atoi(num) * sizeof(int *));
       c = fgetc(file);
-      printf("%c", c);
 
       i = 0;
 
       while (IS_NUM(c)) {
         num[i] = c;
         c = fgetc(file);
-        printf("%c", c);
         ++i;
       }
 
       num[i] = '\0';
+      printf("%s\n", num);
 
       vertices = malloc(atoi(num) * sizeof(int));
 
       while (c != '\n') {
         c = fgetc(file);
-        printf("%c", c);
       }
     }
-  }
+    int e = 0;
 
-  int e = 0;
+    if (c == 'e') {
+      while (!IS_NUM(c)) {
+        c = fgetc(file);
+      }
 
-  if (c == 'e') {
-    while (!IS_NUM(c)) {
+      char num[20];
+      int i = 0;
+
+      while (IS_NUM(c)) {
+        num[i] = c;
+        c = fgetc(file);
+        ++i;
+      }
+
+      num[i] = '\0';
+      printf("%s ", num);
+
+      edges[e] = (int *)malloc(2 * sizeof(int));
+      edges[e][0] = atoi(num);
       c = fgetc(file);
-      printf("%c", c);
+
+      i = 0;
+
+      while (IS_NUM(c)) {
+        num[i] = c;
+        c = fgetc(file);
+        ++i;
+      }
+
+      num[i] = '\0';
+      printf("%s\n", num);
+
+      edges[e][1] = atoi(num);
+
+      while (c != '\n') {
+        c = fgetc(file);
+      }
+      ++e;
     }
-
-    char num[20];
-    int i = 0;
-
-    while (IS_NUM(c)) {
-      num[i] = c;
-      c = fgetc(file);
-      printf("%c", c);
-      ++i;
-    }
-
-    num[i] = '\0';
-
-    edges[e] = (int *)malloc(2 * sizeof(int));
-    edges[e][0] = atoi(num);
-    c = fgetc(file);
-    printf("%c", c);
-
-    i = 0;
-
-    while (IS_NUM(c)) {
-      num[i] = c;
-      c = fgetc(file);
-      printf("%c", c);
-      ++i;
-    }
-
-    num[i] = '\0';
-
-    edges[e][1] = atoi(num);
-
-    while (c != '\n') {
-      c = fgetc(file);
-      printf("%c", c);
-    }
-    ++e;
   }
 }
 
@@ -119,7 +110,7 @@ int main(int argc, char *argv[]) {
   parse(file, vertices, edges);
 
   // printf("%d\n", vertices[0]);
-  printf("%d\n", edges[1][0]);
+  // printf("%d\n", edges[1][0]);
 
   free(file);
   free(edges);
