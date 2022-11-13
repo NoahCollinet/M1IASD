92cbbf4a8aab19ec69bcd05d31e72e1f967b38ec
NoahCollinet <98711686+NoahCollinet@users.noreply.github.com>
2022-11-13 23:07:47 +0100

    ServeurTCP version 2

---
+++ Projet/Parser/serveurTCP.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include <arpa/inet.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+#include<pthread.h>
+#include"parser.h"
+
+
+char client_message[2000];
+char buffer[1024];
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+
+void * socketThread(void *arg)
+{
+  int newSocket = *((int *)arg);
+  recv(newSocket , client_message , 2000 , 0);
+
+  // Send message to the client socket 
+  pthread_mutex_lock(&lock);
+  char *message = malloc(sizeof(client_message)+20);
+  strcpy(message,"Hello Sommet : ");
+  strcat(message,client_message);
+  strcat(message,"\n");
+  strcpy(buffer,message);
+  free(message);
+  pthread_mutex_unlock(&lock);
+  sleep(1);
+  send(newSocket,buffer,13,0);
+  printf("Exit socketThread \n");
+  close(newSocket);
+  pthread_exit(NULL);
+}
+
+
+
+int main(int argc , char *argv[] ){
+
+   if (argc != 2) {
+    fprintf(stderr, "File:", argv[0]);
+    return 1;
+  } 
+  FILE *file = fopen(argv[1], "r"); //Ouverture du fichier
+
+  int *vertices, (*edges)[2], *props; // Création des tableaux
+  parse(file, &vertices, &edges, &props); // Allocation et initialisation des tableaux
+
+   /*
+   for (size_t n = 0; n < props[1]; ++n) { // Affichage des arrêtes
+    printf("%d -> ", edges[n][0]);
+    printf("%d\n", edges[n][1]);
+  } */
+
+  int serverSocket, newSocket;
+  struct sockaddr_in serverAddr;
+  struct sockaddr_storage serverStorage;
+  socklen_t addr_size;
+
+  //Create the socket. 
+  serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+  // Configure settings of the server address struct
+  // Address family = Internet 
+  serverAddr.sin_family = AF_INET;
+
+  //Set port number, using htons function to use proper byte order 
+  serverAddr.sin_port = htons(7799);
+
+  //Set IP address to localhost 
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+
+  //Set all bits of the padding field to 0 
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+
+  //Bind the address struct to the socket 
+  bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+   
+
+  //Listen on the socket, with max connection requests queued = nombre d'arêtes  
+  if(listen(serverSocket,props[1])==0)
+    printf("Listening\n");
+  else
+    printf("Error\n");
+    pthread_t tid[60];
+    int i = 0;
+    while(1)
+    {
+        //Accept call creates a new socket for the incoming connection
+        addr_size = sizeof serverStorage;
+        newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
+
+        //for each client request creates a thread and assign the client request to it to process
+       //so the main thread can entertain next request
+        if( pthread_create(&tid[i++], NULL, socketThread, &newSocket) != 0 )
+           printf("Failed to create thread\n");
+
+        if( i >= 50)
+        {
+          i = 0;
+          while(i < 50)
+          {
+            pthread_join(tid[i++],NULL);
+          }
+          i = 0;
+        }
+    }
+     // Désallocation des pointeurs
+    free(file);
+    free(edges);
+    free(vertices);
+  return 0;
+}
